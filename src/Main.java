d66f1ee2920896e22c459b05947ed8ee6a8b8d53
Tejasva Ukey <tejasvaukey123@gmail.com>
2026-03-31 12:30:39 +0530

    Alphabets

--- src/Main.java
+++ src/Main.java
@@ -145,8 +145,145 @@ class Main{
         }
     }
 
+    static void binarytriangle(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+
+        for (int i = 1; i <= n; i++) {
+            boolean bin = (i%2 != 0);
+            for (int j = 1; j <= i; j++) {
+                if(bin) System.out.print(1);
+                else System.out.print(0);
+
+                bin = !bin;
+            }
+            System.out.println();
+        }
+    }
+
+    static void mShapeNumber(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            for (int j = 0; j < 2*n -2*i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    static void rightTriangleCounting(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+        int num = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+
+    static void rightTriangleCounting2(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int num = 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+
+    static void rightTriangleAlphabet(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            char ch = 'A';
+            for (int j = 0; j <= i; j++) {
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+
+    static void rightTriangleAlphabetReverse(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+
+            for (char ch = 'A';ch <= 'A' + (n-i-1); ch++) {
+                System.out.print(ch);
+
+            }
+            System.out.println();
+        }
+    }
+
+    static void rightTriangleAlphabet2(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+        char ch = 'A';
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(ch+" ");
+
+            }
+            System.out.println();
+            ch++;
+
+        }
+    }
+
+    static void pyramidAlphabet(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                System.out.print(" ");
+            }
+            char ch = 'A';
+            int breakpoint = (2*i+1)/2;
+            for (int j = 1; j <= 2*i+1; j++) {
+                System.out.print(ch);
+                if(j <= breakpoint) ch++;
+                else ch--;
+            }
+            System.out.println();
+
+
+        }
+    }
+
+
+
     static void main(String[] args) {
-        System.out.println("abc");
-        halfDiamond();
+        pyramidAlphabet();
     }
 }
